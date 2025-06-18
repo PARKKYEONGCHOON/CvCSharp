928b91cde16415f487bb2826a0e22680162d2fee
PARKKYEONGCHOON <98440576+PARKKYEONGCHOON@users.noreply.github.com>
2025-06-18 10:20:07 +0900

    Add files via upload

--- BitmapToMat.cs
+++ BitmapToMat.cs
@@ -1,21 +1,38 @@
-public static Mat BitmapToMat(Bitmap bitmap)
+    public static Mat BitmapToMat(Bitmap bitmap)
+    {
+        
+        if (bitmap.PixelFormat != PixelFormat.Format24bppRgb)
         {
-            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
-            BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-
-            try
+            Bitmap converted = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format24bppRgb);
+            using (Graphics g = Graphics.FromImage(converted))
             {
-                int bytes = Math.Abs(bmpData.Stride) * bitmap.Height;
-                byte[] rgbValues = new byte[bytes];
-                Marshal.Copy(bmpData.Scan0, rgbValues, 0, bytes);
-
-                Mat mat = new Mat(bitmap.Height, bitmap.Width, MatType.CV_8UC3);
-                Marshal.Copy(rgbValues, 0, mat.Data, bytes);
-
-                return mat;
+                g.DrawImage(bitmap, new Rectangle(0, 0, bitmap.Width, bitmap.Height));
             }
-            finally
-            {
-                bitmap.UnlockBits(bmpData);
-            }
+
+            bitmap = converted;
         }
+
+        
+        Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+        BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+        try
+        {
+            int stride = bmpData.Stride;
+            int bytes = stride * bitmap.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            
+            Marshal.Copy(bmpData.Scan0, rgbValues, 0, bytes);
+
+           
+            Mat mat = new Mat(bitmap.Height, bitmap.Width, MatType.CV_8UC3);
+            Marshal.Copy(rgbValues, 0, mat.Data, bytes);
+
+            return mat;
+        }
+        finally
+        {
+            bitmap.UnlockBits(bmpData);
+        }
+    }
